67ef3a9e6c3d1cc896d720877ff31d2a333efbb2
ajaidevops <ajai.devops@gmail.com>
2018-11-13 05:47:59 +0000

    add files

---
+++ factorial.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+factorial()
+{
+local=$1
+if((local<=2)); then
+echo $local
+else
+f=$((local-1))
+#recursive call
+f=$(factorial $f)
+f=$((f*local))
+echo $f
+fi
+}
+#main script
+read -p "Enter number:" n
+if ((n==0)); then
+echo 1
+else
+#calling factorial function
+factorial $n
+fi
---
+++ reverse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "enter string:" string
+len=${#string}
+for (( i=$len-1; i>=0; i-- ))
+do
+reverse="$reverse${string:$i:1}"
+done
+echo "$reverse"
